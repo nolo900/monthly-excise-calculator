91f375d623c3f119e8ae8ea5c0f6fbc641d7adb8
Austin Miles <abm@austinbmiles.com>
2016-11-10 18:09:45 -0500

    integrated Moment.js for date calculations

--- js/main.js
+++ js/main.js
@@ -14,42 +14,39 @@ $('.input-group.date').datepicker({
 		});	
 
 
-
 function writeTodaysDate(){
-	$("#todayDate").val(new Date.today().toString("d-MMM-yyyy"));
-	};
+		$("#todayDate").val(new Date.today().toString("d-MMM-yyyy"));
+	}
 
 
 $("#grossSales").keyup(function(){
 	recalculateFields()
 });
 
+function reportMonthChangeHandler(){
+	$("#grossSales").keyup();
+}
+
+function recalculateFields(){
+	calculateTaxDue();
+	calculateVendorCompensation();
+	calculateTotalTaxes();
+	calculatePenalty();
+	calculateInterest();
+	calculateTotalDue()
+}
 
 function pastCutoff(){
-	var cutoffDate = Date.parse($("#reportMonth").val());
-	cutoffDate.addMonths(1).add(19).days();
+	var cutoffDate = moment(Date.parse($("#reportMonth").val()));
+	cutoffDate.add(1,'month').add(19, 'days');	
 
-	if (+Date.today() <= +cutoffDate) {
+	if (+moment($('#todayDate').val()) <= +cutoffDate){
 		return false;
 	} else {
 		return true;
 	}
+	
 }
-
-function reportMonthChangeHandler(){
-	$("#grossSales").keyup();
-};
-
-
-function recalculateFields(){
-	calculateTaxDue()
-	calculateVendorCompensation()
-	calculateTotalTaxes()
-	calculatePenalty()
-	calculateInterest()
-	calculateTotalDue()
-}
-
 
 function calculateTaxDue(){
 	$("#taxDue").val(accounting.toFixed( (.03 * parseFloat($("#grossSales").val())) ,2));
@@ -90,38 +87,23 @@ function calculatePenalty(){
 }
 
 function calculateInterest(){
-	var monthCutoff = Date.parse($("#reportMonth").val()); 
-	var monthsLate = monthDiff(monthCutoff,Date.parse($("#todayDate").val()));
+	var today = moment(Date.parse($('#todayDate').val()));
+	var monthCutoff = moment(Date.parse($("#reportMonth").val())).add(1,'months');
+
+	var monthsLate = today.diff(monthCutoff, 'months');
+
 	var interestPerMonth = 0.00542 * (parseFloat($("#taxDue").val()) + parseFloat($("#penalty").val()));
 	var val = monthsLate * interestPerMonth;
 	
-	$("#interest").val(accounting.toFixed(val,2)); 
+	if (val > 0){
+		$("#interest").val(accounting.toFixed(val,2));
+	}
+
 }
 
 function calculateTotalDue(){
 	var val = parseFloat($("#totalTaxes").val()) +
 			parseFloat($("#penalty").val()) +
-			parseFloat($("#interest").val())
-	$("#totalDue").val(
-			accounting.toFixed(val,2)
-		);
+			parseFloat($("#interest").val());
+	$("#totalDue").val(accounting.toFixed(val,2));
 }
-
-function monthDiff(d1, d2) {
-    var months;
-    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth() + 1;
-    months += d2.getMonth();
-    return months <= 0 ? 0 : months;
-}
-
-// function calculateTaxDue(){
-// 	$("#taxDue").val(.03 * $("#grossSales").val());
-// 	console.log("test")
-// };
-
-
-
-// $("vendorCompensation").val(function(){
-// 	if(true){console.log("test")}
-// });
